facbe468ee8f932240a1e26758745b16423f3c71
kurariha <haiji@gmail.com>
2025-01-19 12:10:10 +0900

    照会機能作成

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+// Memoクラスを読み込む
+use App\Models\Memo;
 use Illuminate\Http\Request;
 
 class MemoController extends Controller
 {
-    //
+    // showページへ移動
+    public function show($id)
+    {
+        $memo = Memo::find($id);
+        return view('memos.show', ['memo' => $memo]);
+    }
 }
