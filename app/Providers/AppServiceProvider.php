277d32face5abbca101be9f13e9a6753658ef7fe
kurariha <haiji@gmail.com>
2025-01-25 09:30:44 +0900

    ILLuminate修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use ILLuminate\Routing\UrlGenerator;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
